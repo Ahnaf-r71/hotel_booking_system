bbb3cd7414859c8f8c4205702245045d0cebef84
Ahnaf-r71 <ahnafr71@gmail.com>
2024-08-30 00:56:17 +0600

    first commit

---
+++ reUse/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="style/header.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+</head>
+<body>
+    <section id="head">
+        <section id="header_background">
+            <div id="logo">
+            <img src="/image/" alt="Logo">
+            </div>
+
+        <div id="menu_list">
+            <ul id="menu_list_content">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="index.php">About Us</a></li>
+                <li><a href="index.php">Booking</a></li>
+                <li><a href="index.php">Travel Blogs</a></li>
+                <li><a href="index.php">Contact Us</a></li>
+                
+            </ul>
+        </div>
+
+        <div id="button">
+            <button id="callForBooking">
+                Call for Booking
+            </button>
+        </div>
+        </section>
+    </section>
+
+</body>
+</html>
